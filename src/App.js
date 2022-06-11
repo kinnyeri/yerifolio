8428d014cb64f0cdc68728c355df73a83c4fa7c5
Kim Yeri <98sena@naver.com>
2022-06-11 21:34:32 +0900

    init with styled-components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    --color-text: pink;
+    --color-background: white;
+    --color-primary: rebeccapurple;
+  }
+`;
+const TestText = styled.div`
+  color: var(--color-text);
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <TestText>Welcome to yeri's polio</TestText>
+    </>
   );
 }
 
