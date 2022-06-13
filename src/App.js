4cabfeeb7e99d8821abc6e2234b715a1d48dc5e5
Kim Yeri <98sena@naver.com>
2022-06-14 02:22:23 +0900

    [feat] 전체 구성 요소 적용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
+import React from "react";
 import styled, { createGlobalStyle } from "styled-components";
+import PageWrapper from "./components/PageWrapper";
+import SideBarBox from "./components/SideBar";
+import Project from "./Project";
+import Skill from "./Skill";
 
 const GlobalStyles = createGlobalStyle`
   html {
-    --color-text: pink;
-    --color-background: white;
-    --color-primary: rebeccapurple;
+    --color-main: #BCAFD4;
   }
 `;
-const TestText = styled.div`
-  color: var(--color-text);
+const OuterPageBox = styled.div`
+  height: 100vh;
+  overflow-y: auto;
 `;
-
+const MainPageBox = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-itmes: center;
+`;
+const ContentPageBox = styled.div`
+  display: flex;
+`;
 function App() {
   return (
     <>
       <GlobalStyles />
-      <TestText>Welcome to yeri's polio</TestText>
+      <OuterPageBox>
+        <MainPageBox></MainPageBox>
+        <ContentPageBox>
+          <SideBarBox />
+          <PageWrapper>
+            <Skill />
+            <Project />
+          </PageWrapper>
+        </ContentPageBox>
+      </OuterPageBox>
     </>
   );
 }
