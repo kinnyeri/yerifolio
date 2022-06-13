15d1b452bae888d0b1fabb96f772583f1e6a8184
Kim Yeri <98sena@naver.com>
2022-06-14 02:22:09 +0900

    [feat] Project 페이지 구현

---
+++ src/Project.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ProjectBox = styled.div`
+  width: 100%;
+  height: 300vh;
+  background-color: blue;
+`;
+const Project = () => {
+  return <ProjectBox>skill</ProjectBox>;
+};
+export default Project;
