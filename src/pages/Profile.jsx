20903e523d71e18c6194d26b3cfa3af07f6beb0a
Kim Yeri <98sena@naver.com>
2022-06-14 04:40:13 +0900

    [feat] Profile 내용 구성

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,82 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+export const Profile = () => {
+  return (
+    <ProfilBox>
+      <ContentBox>
+        <ProfileImgBox>
+          <img src="/myprofile.jpg" alt="프로필 이미지" />
+        </ProfileImgBox>
+        <TitleBox>
+          문제 해결이
+          <br />
+          즐거운 김예리입니다.
+        </TitleBox>
+        <SubTitleBox>
+          프론트엔드 개발자를 희망하며
+          <br />
+          어려운 난관을 헤쳐가며 얻는 경험과 배움을 좇습니다.
+          <br />
+          언제나 도전하는 사람이 되고자 합니다.
+        </SubTitleBox>
+      </ContentBox>
+    </ProfilBox>
+  );
+};
 
 const ProfilBox = styled.div`
   height: 100vh;
+
   display: flex;
-  justify-content: center;
-  align-itmes: center;
+  align-items: center;
 `;
-export const Profile = () => {
-  return <ProfilBox>skill</ProfilBox>;
-};
+const ContentUp = keyframes`
+  0% {
+    -webkit-transform: translateY(50px);
+            transform: translateY(50px);
+    -webkit-transform-origin: 50% 50%;
+            transform-origin: 50% 50%;
+    text-shadow: none;
+  }
+  100% {
+    -webkit-transform: translateY(0);
+            transform: translateY(0);
+    -webkit-transform-origin: 50% 50%;
+            transform-origin: 50% 50%;
+  }
+`;
+const ContentBox = styled.div`
+  width: 100%;
+  height: 80%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+
+  animation: ${ContentUp} 1s 1 ease-in normal;
+`;
+const ProfileImgBox = styled.div`
+  width: 20vw;
+  height: 20vw;
+  border-radius: 100%;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+  animation: ${ContentUp} 0.3s 1 ease-in normal;
+`;
+const TitleBox = styled.div`
+  font-size: 70px;
+  line-height: 75px;
+  text-align: center;
+
+  animation: ${ContentUp} 0.5s 1 ease-in normal;
+`;
+const SubTitleBox = styled.div`
+  font-size: 22px;
+  text-align: center;
+
+  animation: ${ContentUp} 0.8s 1 ease-in normal;
+`;
