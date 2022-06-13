cc95ee7ffeb1ecf7f26de40ab777a19a5c6f6d4b
Kim Yeri <98sena@naver.com>
2022-06-14 02:21:30 +0900

    [feat] Skill 페이지 구현

---
+++ src/Skill.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const SkillBox = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: yellow;
+`;
+const Skill = () => {
+  return <SkillBox>skill</SkillBox>;
+};
+export default Skill;
