0c2fa88f705d82dd7fdc3d41adde0fc9db47ab6a
Kim Yeri <98sena@naver.com>
2022-08-16 00:47:00 +0900

    [feat] 파일 뷰어 버튼 생성

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,25 +1,50 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { HighLight } from "./HighLight";
 export const Content = ({ title, details }) => {
+  const [modal, setModal] = useState(false);
+  // {link !== undefined ? (
+  //   <Detail>
+  //     <a href={link} target="_blank">
+  //       {content}
+  //     </a>
+  //   </Detail>
+  // ) : (
+  //   <Detail>{content}</Detail>
+  // )}
   return (
     <ContentContainer>
       <TitleBox>
         <HighLight>{title}</HighLight>
       </TitleBox>
       <DetailsContainer>
-        {details.map(({ icon, content, link }, idx) => (
+        {details.map(({ type, icon, content, link, file, fileName }, idx) => (
           <DetailContainer key={idx}>
             <IconBox>{icon}</IconBox>
             <DetailBox>
-              {link !== undefined ? (
-                <Detail>
-                  <a href={link} target="_blank">
-                    {content}
-                  </a>
-                </Detail>
-              ) : (
-                <Detail>{content}</Detail>
-              )}
+              {
+                {
+                  link: (
+                    <Detail>
+                      <a href={link} target="_blank">
+                        {content}
+                      </a>
+                    </Detail>
+                  ),
+                  default: <Detail>{content}</Detail>,
+                  file: (
+                    <Detail>
+                      <button>{content}</button>
+                    </Detail>
+                  ),
+                  withFile: (
+                    <Detail>
+                      <span>{content} </span>
+                      <button>{fileName}</button>
+                    </Detail>
+                  ),
+                }[type]
+              }
             </DetailBox>
           </DetailContainer>
         ))}
